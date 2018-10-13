a5a57c4e735a8e3b118a625a34a2a1f1550792a6
Udeshika Kaushalya <udeshikaranaweera1994@gmail.com>
2018-10-13 21:01:53 +0530

    First Commit

---
+++ OAuth_2.0/instagram-api-settings.php
@@ -0,0 +1,9 @@
+<?php
+
+define('INSTAGRAM_CLIENT_ID', '9ea5035fdf954f6abef7b3c3bcab8500');
+
+define('INSTAGRAM_CLIENT_SECRET', '4105b18e0f834e07a1063a4fefdfc648');
+
+define('INSTAGRAM_REDIRECT_URI', 'http://localhost/OAuth_2.0/instagram-auth.php');
+
+?>
